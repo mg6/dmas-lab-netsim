7209946ca6737b49a3fde6edda7c6c0c5378cd2a
Daroslaw <daroslawowicz@gmail.com>
2018-01-13 13:52:28 +0100

    Included bandwidth and packet counting in generator.

--- generator.cc
+++ generator.cc
@@ -8,16 +8,20 @@ class Generator : public cSimpleModule {
     virtual void handleMessage(cMessage *msg) override;
 
     int packetLength;
-    int sendRate;
     int destination;
+    simtime_t secondsPerPacket;
+
+    int numPacketsEmitted;
 };
 
 Define_Module(Generator);
 
 void Generator::initialize(){
     packetLength = par("packetLength");
-    sendRate = par("sendRate");
+    long sendRate = par("sendRate");
+    secondsPerPacket = packetLength * (8.f / (float)sendRate);
     destination = 0;
+    numPacketsEmitted = 0;
     cMessage *selfMessage = new cMessage();
     scheduleAt(simTime(), selfMessage);
 }
@@ -26,14 +30,26 @@ void Generator::handleMessage(cMessage *msg){
     Packet *pkt = new Packet();
     pkt->setDestinationAddress(destination);
     pkt->setByteLength((int64_t)packetLength);
+    char name[] = "Packet             ";
+    itoa(numPacketsEmitted, name + 7, 10);
+    pkt->setName(name);
+
     cChannel *outChannel = gate("out")->getTransmissionChannel();
     simtime_t finishTime = outChannel->getTransmissionFinishTime();
-    if(finishTime <= simTime())
+    simtime_t varSimTime = simTime();
+    simtime_t timeDiff = secondsPerPacket - ((finishTime<0)?0:finishTime - varSimTime);
+
+    if(finishTime <= varSimTime)
     {
         send(pkt, "out");
         EV << "Packet sent to " << pkt->getDestinationAddress() << " at time " << simTime() << ".\n";
         destination = (++destination)%4;
-        scheduleAt(simTime(), msg);
+        numPacketsEmitted++;
+
+        if(timeDiff > secondsPerPacket)
+            scheduleAt(varSimTime + secondsPerPacket, msg);
+        else
+            scheduleAt(varSimTime + timeDiff, msg);
     }
     else
     {
