d70c7df5b503359e7f1513e6c6cd2295a743075f
Mateusz <mateusz.michalik.poczta@gmail.com>
2018-01-16 20:30:28 +0100

    packet 2 issue

--- analyzer.cc
+++ analyzer.cc
@@ -11,25 +11,32 @@ class Analyzer : public cSimpleModule {
     virtual ~Analyzer();
     virtual void handleMessage(cMessage *msg) override;
     virtual void refreshDisplay() const override;
-    void StartPackedAnalyze(cMessage *msg);
-    void SendAnalyzedMessageFurther(cMessage *msg);
+    void StartPackedAnalyze();
+    void EndAnalyze();
     void DisplayPackagesData();
+    bool isAnalyzing = false;
     float analyzeTime;
     unsigned long receivedMessages = 0;
-    unsigned long droppedMessages=0;
-    unsigned long analyzedMessages=0;
-    unsigned long startedAnalyzeMessages=0;
+    unsigned long droppedMessages = 0;
+    unsigned long analyzedMessages = 0;
+    unsigned long startedAnalyzeMessages = 0;
     Queue queue;
-    char analyze[2]="a";
+    char analyze[2] = "a";
+
+    Queue queueOut;
+    void TryToSendMessage();
+    simtime_t getInterfaceDelay();
+    bool isSendingMessages = false;
+    char selfString[2] = "s";
 
 };
 
 Define_Module(Analyzer);
 
-void Analyzer::initialize(){
-    int usTime=par("analyzeTime");
-analyzeTime=(float)usTime/usInS;
-EV_INFO << "t: " << analyzeTime<< endl;
+void Analyzer::initialize() {
+    int usTime = par("analyzeTime");
+    analyzeTime = (float)usTime / usInS;
+    EV_INFO << "t: " << analyzeTime << endl;
 }
 
 Analyzer::~Analyzer() {
@@ -42,51 +49,124 @@ Analyzer::~Analyzer() {
 void Analyzer::handleMessage(cMessage *msg)
 {
     ++receivedMessages;
-   if(strncmp(msg->getName(), analyze, 10 )==0)    //if(msg->isSelfMessage())
+    EV_INFO << "0" << endl;
+    if (strncmp(msg->getName(), analyze, 10) == 0)    //if(msg->isSelfMessage())
     {
-       SendAnalyzedMessageFurther(msg);
+        EV_INFO << "1" << endl;
+        delete msg;
+        EndAnalyze();
+    }
+    else if (strncmp(msg->getName(), selfString, 10) == 0)
+    {
+        EV_INFO << "2 " << endl;
+        delete(msg);
+        TryToSendMessage();
     }
     else
     { //receiving from sdn
         DisplayPackagesData();
-
-               if(!queue.IsFull())
-               {
-                   Packet *pack = check_and_cast<Packet*>(msg);
-                   queue.enqueue(pack);
-                   StartPackedAnalyze(msg);
-               }
-               else
-               {
-                   droppedMessages++;
-                   EV_INFO << "dropped packed" << endl;
-                   delete msg;
-               }
+        EV_INFO << "1" << endl;
+        if (!queue.IsFull())
+        {
+            Packet *pack = check_and_cast<Packet*>(msg);
+            queue.enqueue(pack);
+            if (!isAnalyzing)
+            {
+                StartPackedAnalyze();
+            }
+        }
+        else
+        {
+            droppedMessages++;
+            EV_INFO << "dropped packed" << endl;
+            delete msg;
+        }
     }
-
+    EV_INFO << "099" << endl;
 }
 
-void Analyzer::StartPackedAnalyze(cMessage *msg)
+void Analyzer::StartPackedAnalyze()
 {
+    EV_INFO << "4.6" << endl;
+    isAnalyzing = true;
     EV_INFO << "Started packed analyze" << endl;
     startedAnalyzeMessages++;
     cMessage *delayEvent = new cMessage("delayEvent");
     delayEvent->setName(analyze);
-    scheduleAt(simTime()+analyzeTime, delayEvent);
+    EV_INFO << "4.7" << endl;
+    scheduleAt(simTime() + analyzeTime, delayEvent);
+    EV_INFO << "4.8" << endl;
 }
-void Analyzer::SendAnalyzedMessageFurther(cMessage *msg)
+void Analyzer::EndAnalyze()
 {
-        analyzedMessages++;
-       delete msg;
-       send(queue.dequeue(), "out");
-       EV_INFO << "ended analyze " << endl;
+    analyzedMessages++;
+    //send(queue.dequeue(), "out");
+    EV_INFO << "3" << endl;
+
+
+
+    if (!isSendingMessages && !queueOut.IsFull())
+    {
+        EV_INFO << "4" << endl;
+        isSendingMessages = true;
+        queueOut.enqueue(queue.dequeue());
+        TryToSendMessage();
+        EV_INFO << "10" << endl;
+    }
+
+    EV_INFO << "ended analyze " << endl;
+    if (!queue.IsEmpty())
+    {
+        EV_INFO << "4.5" << endl;
+        StartPackedAnalyze();
+    }
+    else
+        isAnalyzing = false;
+    //move it to the moment package is pushed forward as it is guarantee we can start another analyze
+
+
 }
+
+void Analyzer::TryToSendMessage() {
+    simtime_t txEnd = getInterfaceDelay();
+    if (txEnd <= simTime()) {
+        EV_INFO << "5" << endl;
+        send(queueOut.dequeue(), "out");
+        EV_INFO << "6" << endl;
+        if (!queueOut.IsEmpty()) {
+            EV_INFO << "7" << endl;
+            simtime_t txEnd = getInterfaceDelay();
+            cMessage *self = new cMessage("self");
+            self->setName(selfString);
+            scheduleAt(txEnd > simTime() ? txEnd : simTime(), self);
+            EV_INFO << "8" << endl;
+
+        }
+        else {
+            isSendingMessages = false;
+        }
+    }
+    else
+    {
+        EV_INFO << "9" << endl;
+        TryToSendMessage();
+    }
+}
+
+simtime_t Analyzer::getInterfaceDelay() {
+    simtime_t txFT;
+    cChannel *txChn = gate("out")->getTransmissionChannel();
+    txFT = txChn->getTransmissionFinishTime();
+    return txFT;
+}
+
+
 void Analyzer::DisplayPackagesData()
 {
-        EV_INFO << "received fresh data" << endl;
-        EV_INFO << "dropped: " <<droppedMessages<< endl;
-        EV_INFO << "analyzed: " <<analyzedMessages<< endl;
-        EV_INFO << "AnalyzeMessages: " <<startedAnalyzeMessages<< endl;
+    EV_INFO << "received fresh data" << endl;
+    EV_INFO << "dropped: " << droppedMessages << endl;
+    EV_INFO << "analyzed: " << analyzedMessages << endl;
+    EV_INFO << "AnalyzeMessages: " << startedAnalyzeMessages << endl;
 }
 
 void Analyzer::refreshDisplay() const {
--- sdn.cc
+++ sdn.cc
@@ -3,9 +3,13 @@
 using namespace omnetpp;
 
 class Sdn : public cSimpleModule {
-
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
+    void TryToSendMessage();
+    Queue queue;
+    simtime_t getInterfaceDelay();
+    bool isSendingMessages=false;
+    char selfString[2]="s";
 };
 
 Define_Module(Sdn);
@@ -14,5 +18,52 @@ void Sdn::initialize(){
 }
 
 void Sdn::handleMessage(cMessage *msg){
-    send(msg, "out");
+    if(strncmp(msg->getName(), selfString, 10 )==0) //self message
+    {
+      delete(msg);
+      TryToSendMessage();
+    }
+else if(!queue.IsFull())
+                   {
+
+                       Packet *pack = check_and_cast<Packet*>(msg);
+                       queue.enqueue(pack);
+                       if(!isSendingMessages)
+                       {
+                           isSendingMessages=true;
+                           TryToSendMessage();
+                       }
+                   }
+
 }
+
+void Sdn::TryToSendMessage(){
+    simtime_t txEnd = getInterfaceDelay();
+                if(txEnd <= simTime()){
+                    send(queue.dequeue(), "out");
+                    if(!queue.IsEmpty()){
+                        simtime_t txEnd = getInterfaceDelay();
+                        cMessage *self = new cMessage("self");
+                            self->setName(selfString);
+                        scheduleAt(txEnd>simTime()?txEnd:simTime(), self);
+                                        }
+                    else {
+                        isSendingMessages=false;
+                    }
+                }
+                else
+                {
+                    TryToSendMessage();
+                }
+}
+
+simtime_t Sdn::getInterfaceDelay(){
+            simtime_t txFT;
+            cChannel *txChn = gate("out")->getTransmissionChannel();
+            txFT = txChn->getTransmissionFinishTime();
+             return txFT;
+}
+
+
+
+
